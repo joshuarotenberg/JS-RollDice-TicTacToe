59a242d175fcc6a0e40e844be9486a99687381d2
Joshua Rotenberg <joshua.rotenberg@gmail.com>
2020-10-16 14:10:17 -0400

    stuck on winner logic

--- tictactoe/js/app.js
+++ tictactoe/js/app.js
@@ -25,7 +25,6 @@ document
     let gameStarted = false;
 
     let player = "";
-    let turn = 0;
 
     document
     .getElementById("start-game")
@@ -45,16 +44,19 @@ document
         if (gameStarted) {
             console.log("Game started");
 
+            let turn = 0    ;
+
             // create new Array with 9 spots where value is null for each
             const selectedBoxes = new Array(9).fill(null);
 
             document
             .getElementById("game-board")
             .addEventListener("click", function(e) {
+
                 turn += 1;
                 var boxSelected = e.target.id;
                 
-                console.log(`clicking box ${boxSelected}`);
+
                 
                 var boxSwap = document
                     .getElementById(boxSelected);
@@ -67,67 +69,79 @@ document
                 }
 
                 // set player and update div with player marker
-                if(turn % 2 == 1) {
+                if(turn % 2 == 0) {
                     if (selectedBoxes.filter == "x" || selectedBoxes.filter == "o") {
                         return;
                     }
-                    selectedBoxes[boxSelected] = "x";
-                    player = "Player 1";
-                    boxSwap.innerHTML = '<div class="x"></div>';
-                } else {
                     selectedBoxes[boxSelected] = "o";
                     player = "Player 2";
+
+                    console.log(`turn ${turn}`);
+                    console.log(`player: ${player}`);
+                    console.log(`clicking box ${boxSelected}`);
+
                     boxSwap.innerHTML = '<div class="circle"></div>';
+                } else {
+                    selectedBoxes[boxSelected] = "x";
+                    player = "Player 1";
+
+                    console.log(`turn ${turn}`);
+                    console.log(`player: ${player}`);
+                    console.log(`clicking box ${boxSelected}`);
+
+                    boxSwap.innerHTML = '<div class="x"></div>';
+                    
                 }
+
 
                 // Alert Winner log winning combo
                 if (selectedBoxes[0] == "x" && selectedBoxes[1] == "x" && selectedBoxes[2] == "x") {
-                    alert("Congrats Player 1");
+                    console.log("Congrats Player 1");
                     console.log(`winning combo ${selectedBoxes}`);
                 } else if (selectedBoxes[0] == "o" && selectedBoxes[1] == "o" && selectedBoxes[2] == "o") {
-                    alert("Congrats Player 2");
+                    console.log("Congrats Player 2");
                     console.log(`winning combo ${selectedBoxes}`);
                 } else if (selectedBoxes[3] == "x" && selectedBoxes[4] == "x" && selectedBoxes[5] == "x") {
-                    alert("Congrats Player 1");
+                    console.log("Congrats Player 1");
                     console.log(`winning combo ${selectedBoxes}`);
                 }  else if (selectedBoxes[3] == "o" && selectedBoxes[4] == "o" && selectedBoxes[5] == "o") {
-                    alert("Congrats Player 2");
+                    console.log("Congrats Player 2");
                     console.log(`winning combo ${selectedBoxes}`);
                 }  else if (selectedBoxes[6] == "x" && selectedBoxes[7] == "x" && selectedBoxes[8] == "x") {
-                    alert("Congrats Player 1");
+                    console.log("Congrats Player 1");
                     console.log(`winning combo ${selectedBoxes}`);
                 }  else if (selectedBoxes[6] == "o" && selectedBoxes[7] == "o" && selectedBoxes[8] == "o") {
-                    alert("Congrats Player 2");
+                    console.log("Congrats Player 2");
                     console.log(`winning combo ${selectedBoxes}`);
                 }  else if (selectedBoxes[0] == "x" && selectedBoxes[3] == "x" && selectedBoxes[6] == "x") {
-                    alert("Congrats Player 1");
+                    console.log("Congrats Player 1");
                     console.log(`winning combo ${selectedBoxes}`);
                 }  else if (selectedBoxes[0] == "o" && selectedBoxes[3] == "o" && selectedBoxes[6] == "o") {
-                    alert("Congrats Player 2");
+                    console.log("Congrats Player 2");
                     console.log(`winning combo ${selectedBoxes}`);
                 }  else if (selectedBoxes[1] == "x" && selectedBoxes[4] == "x" && selectedBoxes[7] == "x") {
-                    alert("Congrats Player 1");
+                    console.log("Congrats Player 1");
                     console.log(`winning combo ${selectedBoxes}`);
                 }  else if (selectedBoxes[1] == "o" && selectedBoxes[4] == "o" && selectedBoxes[7] == "o") {
-                    alert("Congrats Player 2");
+                    console.log("Congrats Player 2");
                     console.log(`winning combo ${selectedBoxes}`);
                 }  else if (selectedBoxes[2] == "x" && selectedBoxes[5] == "x" && selectedBoxes[8] == "x") {
-                    alert("Congrats Player 1");
+                    console.log("Congrats Player 1");
                     console.log(`winning combo ${selectedBoxes}`);
                 }  else if (selectedBoxes[2] == "o" && selectedBoxes[5] == "o" && selectedBoxes[8] == "o") {
-                    alert("Congrats Player 2");
+                    console.log("Congrats Player 2");
                     console.log(`winning combo ${selectedBoxes}`);
                 }  else if (selectedBoxes[0] == "x" && selectedBoxes[4] == "x" && selectedBoxes[8] == "x") {
-                    alert("Congrats Player 1");
+                    console.log("Congrats Player 1");
                     console.log(`winning combo ${selectedBoxes}`);
                 }  else if (selectedBoxes[0] == "o" && selectedBoxes[4] == "o" && selectedBoxes[8] == "o") {
-                    alert("Congrats Player 2");
+                    console.log("Congrats Player 2");
                     console.log(`winning combo ${selectedBoxes}`);
                 } else if (selectedBoxes[2] == "x" && selectedBoxes[4] == "x" && selectedBoxes[6] == "x") {
-                    alert("Congrats Player 1");
+                    console.log("Congrats Player 1");
                     console.log(`winning combo ${selectedBoxes}`);
                 }  else if (selectedBoxes[2] == "o" && selectedBoxes[4] == "o" && selectedBoxes[6] == "o") {
-                    alert("Congrats Player 2");
+                    console.log("Congrats Player 2");
                     console.log(`winning combo ${selectedBoxes}`);
                  } 
 
